121d1927587077991f4175d77ee29bff65b84a61
Matvey Kraposhin <mkraposhin@gmail.com>
2025-07-22 17:59:22 +0300

    test 7-10 have been added

--- assignment_tests.cpp
+++ assignment_tests.cpp
@@ -1,9 +1,12 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 using namespace std;
+const static double meps = numeric_limits<double>::epsilon();
+const static double mind = 2.0*numeric_limits<double>::min();
 
 /// Declares an interface for representing a quadratic equation
 class QuadraticEquationI {
@@ -17,7 +20,7 @@ public:
 
     /// Returns a vector with the roots of the quadratic equation
     /// given by coefficients a, b and c
-    virtual vector<double> roots (double a,
+    virtual vector<double> solve (double a,
         double b,
         double c) = 0;
 };
@@ -26,11 +29,11 @@ public:
 /// mock framework
 class QuadraticEquationMock : public QuadraticEquationI {
 public:
-    MOCK_METHOD(vector<double>, roots, (double a, double b, double c));
+    MOCK_METHOD(vector<double>, solve, (double a, double b, double c));
 };
 
 /// The class containing sequential approximations to
-/// "roots" functions for the computation of a quadratic
+/// "solve" functions for the computation of a quadratic
 /// equation's solution.
 template <typename RImpl>
 class QuadraticEquationImpl : public QuadraticEquationI {
@@ -41,20 +44,20 @@ private:
 
      /// An instance of the function to compute a quadratic
      /// equation's roots
-     RImpl roots_impl_;
+     RImpl solve_impl_;
 
 public:
 
     /// Creates new instance of QuadraticEquationImpl and sets
-    /// the roots function implementation
+    /// the solve function implementation
     QuadraticEquationImpl(RImpl r)
-    : QuadraticEquationI(), roots_impl_(r){}
+    : QuadraticEquationI(), solve_impl_(r){}
 
-    /// Returns roots computed by the current implementation of
+    /// Returns solve computed by the current implementation of
     /// root's finding function
-    vector<double> roots (double a,
+    vector<double> solve (double a,
         double b, double c) override final {
-            return roots_impl_(a, b, c);
+            return solve_impl_(a, b, c);
     }
 };
 
@@ -74,16 +77,16 @@ protected:
 /// is conducted using a mock object.
 TEST(QuadraticEquationTest, Test_Item_3) {
     QuadraticEquationMock qemock;
-    EXPECT_CALL(qemock, roots(1.0, 0.0, 1.0))
+    EXPECT_CALL(qemock, solve(1.0, 0.0, 1.0))
         .Times(1)
         .WillOnce(testing::Return(vector<double>{}));
     
-    EXPECT_EQ(qemock.roots(1.0, 0.0, 1.0).size(), 0);
+    EXPECT_EQ(qemock.solve(1.0, 0.0, 1.0).size(), 0);
 }
 
 /// Checks that when a = 1, b = 0 and c = 1 roots are not returned
 /// which corresponds to the next equation: x^2 + 1 = 0. The test
-/// includes a minimal implementation of the roots function.
+/// includes a minimal implementation of the solve function.
 TEST(QuadraticEquationTest, Test_Item_4) {
     auto root_impl = [](double a, double b, double c){
         double D = b*b - 4.0*a*c;
@@ -93,25 +96,26 @@ TEST(QuadraticEquationTest, Test_Item_4) {
         return vector<double>{0.0, 0.0};
     };
     QuadraticEquationImpl qeimpl(root_impl);
-    EXPECT_EQ(qeimpl.roots(1.0, 0.0, 1.0).size(), 0);
-    EXPECT_NE(qeimpl.roots(1.0, 0.0, -1.0).size(), 0);
+    EXPECT_EQ(qeimpl.solve(1.0, 0.0, 1.0).size(), 0);
+    EXPECT_NE(qeimpl.solve(1.0, 0.0, -1.0).size(), 0);
 }
 
-/// Checks if a = 1, b = 0 and c = -1, then there is a root x = 1,
-/// which corresponds to the next equation: x^2 -1 1 = 0. The test
-/// is conducted using a mock object.
+/// Checks if a = 1, b = 0 and c = -1, then there are roots x = 1
+/// and x = -1 which correspond to the next equation: x^2 - 1 = 0.
+// The test is conducted using a mock object.
 TEST(QuadraticEquationTest, Test_Item_5) {
     QuadraticEquationMock qemock;
-    EXPECT_CALL(qemock, roots(1.0, 0.0, -1.0))
+    EXPECT_CALL(qemock, solve(1.0, 0.0, -1.0))
         .Times(1)
         .WillOnce(testing::Return(vector<double>{1.0, 1.0}));
     
-    EXPECT_EQ(qemock.roots(1.0, 0.0, -1.0), (vector<double>{1.0, 1.0}));
+    EXPECT_EQ(qemock.solve(1.0, 0.0, -1.0), (vector<double>{1.0, 1.0}));
 }
 
-/// Checks if a = 1, b = 0 and c = -1, then there is a root x = 1,
-/// which corresponds to the next equation: x^2 -1 1 = 0. The test
-/// includes a minimal implementation of the roots function.
+/// Checks if a = 1, b = 0 and c = -1, then there are roots x = 1
+/// and x = -1 which corresponds to the next equation: x^2 - 1 = 0.
+/// The test
+/// includes a minimal implementation of the solve function.
 TEST(QuadraticEquationTest, Test_Item_6) {
     auto root_impl = [](double a, double b, double c){
         double D = b*b - 4.0*a*c;
@@ -127,13 +131,95 @@ TEST(QuadraticEquationTest, Test_Item_6) {
         return vector<double>{0.0};
     };
     QuadraticEquationImpl qeimpl(root_impl);
-    EXPECT_EQ(qeimpl.roots(1.0, 0.0, -1.0).size(), 2);
-    EXPECT_EQ(qeimpl.roots(1.0, 0.0, -1.0), (std::vector<double>{1.0, -1.0}));
+    EXPECT_EQ(qeimpl.solve(1.0, 0.0, -1.0).size(), 2);
+    EXPECT_EQ(qeimpl.solve(1.0, 0.0, -1.0), (std::vector<double>{1.0, -1.0}));
+}
+
+/// Checks if a = 1, b = 2 and c = 1, then there is a root x = -1
+/// which correspond to the next equation: x^2 + 2x + 1 = 0.
+// The test is conducted using a mock object.
+TEST(QuadraticEquationTest, Test_Item_7) {
+    QuadraticEquationMock qemock;
+    EXPECT_CALL(qemock, solve(1.0, 2.0, 1.0))
+        .Times(1)
+        .WillOnce(testing::Return(vector<double>{-1.0}));
+    
+    EXPECT_EQ(qemock.solve(1.0, 2.0, 1.0), (vector<double>{-1.0}));
+}
+
+/// Checks if a = 1, b = 2 and c = 1, then there is a root x = -1
+/// which corresponds to the next equation: x^2 - 1 = 0.
+/// The test
+/// includes a minimal implementation of the solve function.
+TEST(QuadraticEquationTest, Test_Item_8) {
+    auto root_impl = [](double a, double b, double c){
+        double D = b*b - 4.0*a*c;
+        if (D < 0.0) {
+            return vector<double>{};
+        }
+        if (D > 0.0) {
+            return vector<double>{
+                (-b + sqrt(D) / 2.0 / a),
+                (-b - sqrt(D) / 2.0 / a),
+            };
+        }
+        return vector<double>{-b / 2.0 / a};
+    };
+    QuadraticEquationImpl qeimpl(root_impl);
+    EXPECT_EQ(qeimpl.solve(1.0, 2.0, 1.0).size(), 1);
+    EXPECT_EQ(qeimpl.solve(1.0, 2.0, 1.0), (std::vector<double>{-1.0}));
+}
+
+/// Checks that 'a' is not equal to 0.
+/// In this case we have a degenerated case when
+/// the expression for roots is divided by zero.
+TEST(QuadraticEquationTest, Test_Item_9) {
+    QuadraticEquationMock qemock;
+    EXPECT_CALL(qemock, solve(mind, 2.0, 1.0))
+        .Times(1)
+        .WillOnce(testing::Throw(std::runtime_error("a equals to 0")));
+    
+    EXPECT_THROW(qemock.solve(mind, 2.0, 1.0), std::runtime_error);
+}
+
+///
+///
+///
+TEST(QuadraticEquationTest, Test_Item_10) {
+    auto root_impl = [](double a, double b, double c){
+        if (abs(a) <= mind) {
+            throw std::runtime_error("a equals to 0");
+            return vector<double>{};
+        }
+        double D = b*b - 4.0*a*c;
+        if (D < 0.0) {
+            return vector<double>{};
+        }
+        if (D > 0.0) {
+            return vector<double>{
+                (-b + sqrt(D) / 2.0 / a),
+                (-b - sqrt(D) / 2.0 / a),
+            };
+        }
+        return vector<double>{-b / 2.0 / a};
+    };
+    QuadraticEquationImpl qeimpl(root_impl);
+    EXPECT_EQ(qeimpl.solve(1.0, 2.0, 1.0).size(), 1);
+    EXPECT_THROW(qeimpl.solve(0.0, 2.0, 1.0), std::runtime_error);
+    EXPECT_THROW(qeimpl.solve(mind, 2.0, 1.0), std::runtime_error);
+    vector v = qeimpl.solve(1.0e-6, 0.0, -1.0);
+    std::cout << v[0] << "," << v[1] << std::endl;
 }
 
 int main (int argc, char * argv[])
 {
     testing::InitGoogleMock(&argc, argv);
+
+    std::cout << meps << std::endl;
+    std::cout << bool((1.0 + meps*double(1.0)) == double(1.0)) << std::endl;
+    std::cout << bool((1.0 + meps*double(0.5)) == double(1.0)) << std::endl;
+    std::cout << bool((1.0 + meps*double(2.0)) == double(1.0)) << std::endl;
+
     return RUN_ALL_TESTS();
 }
 
